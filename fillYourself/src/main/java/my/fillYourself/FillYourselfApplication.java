657a747889a77b8c2b35193a695a651265100731
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2024-10-25 04:27:36 +0900

    feat : initial commit

---
+++ fillYourself/src/main/java/my/fillYourself/FillYourselfApplication.java
@@ -0,0 +1,13 @@
+package my.fillYourself;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FillYourselfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FillYourselfApplication.class, args);
+	}
+
+}
---
+++ fillYourself/src/test/java/my/fillYourself/FillYourselfApplicationTests.java
@@ -0,0 +1,13 @@
+package my.fillYourself;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FillYourselfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
